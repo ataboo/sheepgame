87cbe4b86d368fd12aa631ecd1ae8e614f6856dd
ataboo <bmtoolbox@gmail.com>
2016-11-12 13:28:08 -0700

    Added mats

---
+++ Assets/Scripts/Mono/Spooker.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spooker : MonoBehaviour {
+	public bool permaSpooky = false;
+
+	public Material activeMat;
+	public Material innactiveMat;
+	private bool active;
+	private float spookspiry = 0;
+	private Renderer rend;
+
+	public void Start() {
+		active = permaSpooky;
+
+		rend = GetComponent<Renderer>();
+		rend.material = active ?  activeMat : innactiveMat;
+	}
+
+	public void Update() {
+		if (permaSpooky || !active) {
+			return;
+		}
+
+		rend.material = active ?  activeMat : innactiveMat;
+		checkForExpiry();
+	}
+
+	public void Activate(float lifeSecs) {
+		this.active = true;
+
+		spookspiry = Time.time + lifeSecs;
+	}
+
+	public bool IsActive() {
+		return active;
+	}
+	private void checkForExpiry() {
+		if (spookspiry <= Time.time) {
+			this.active = false;
+		}
+	}
+}
