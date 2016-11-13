2546f36640c33fb72d864d6ce88fc28f60eaadfc
ataboo <bmtoolbox@gmail.com>
2016-11-12 21:51:24 -0700

    Adding dog controls

--- Assets/Scripts/Mono/Spooker.cs
+++ Assets/Scripts/Mono/Spooker.cs
@@ -3,18 +3,12 @@ using System.Collections;
 
 public class Spooker : MonoBehaviour {
 	public bool permaSpooky = false;
-
-	public Material activeMat;
-	public Material innactiveMat;
 	private bool active;
 	private float spookspiry = 0;
 	private Renderer rend;
 
 	public void Start() {
 		active = permaSpooky;
-
-		rend = GetComponent<Renderer>();
-		rend.material = active ?  activeMat : innactiveMat;
 	}
 
 	public void Update() {
@@ -22,11 +16,10 @@ public class Spooker : MonoBehaviour {
 			return;
 		}
 
-		rend.material = active ?  activeMat : innactiveMat;
 		checkForExpiry();
 	}
 
-	public void Activate(float lifeSecs) {
+	public void Activate(float lifeSecs = 0) {
 		this.active = true;
 
 		spookspiry = Time.time + lifeSecs;
