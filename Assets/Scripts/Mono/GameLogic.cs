594652db115b6aa56b0e899d94beff777a45c14e
ataboo <bmtoolbox@gmail.com>
2016-12-28 22:25:58 -0700

    Added Sheep and Hud

---
+++ Assets/Scripts/Mono/GameLogic.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+
+public class GameLogic : MonoBehaviour, CountListener, DeathListener {
+	public static PhotonView photonView;
+	
+	public int totalSheep = 0;
+	public int sheepInGoal = 0;
+	public int deadSheep = 0;
+	public UIInterface uiInterface;
+
+	private bool paused = false;
+	private EntitySpawner spawner;
+	private float startTime;
+	private List<GameObject> entities;
+
+	public void Awake() {
+		GameLogic.photonView = GetComponent<PhotonView> ();
+		this.spawner = GetComponent<EntitySpawner> ();
+	}
+
+	public void Start() {
+		InitValues ();
+		this.totalSheep = spawner.sheepCount;
+		SheepCounter sheepCounter = GameObject.FindGameObjectWithTag ("sheep-target").GetComponent<SheepCounter> ();
+		sheepCounter.SetListener (this);
+	}
+
+	public void ExitToMenu() {
+		SetPause(false);
+		SceneManager.LoadScene("MenuScene");
+	}
+		
+	[PunRPC]
+	public void OnCountChange(int newCount) {
+		this.sheepInGoal = newCount;
+
+		UpdateHud ();
+
+		if (PhotonNetwork.isMasterClient) {
+			CheckEnd ();
+		}
+	}
+
+	//============== DeathListener ================
+	public void IsKill(GameObject gameObject, bool respawnable) {
+		if (respawnable) {
+			spawner.RespawnDog(gameObject);
+		} else {
+			DestroyImmediate (gameObject);
+			deadSheep++;
+
+			GetEntities (true);
+			UpdateHud ();
+		}
+	}
+
+	//============== IGameState ================
+	public bool IsPaused() {
+		return paused;
+	}
+
+	private void InitValues() {
+		deadSheep = 0;
+		startTime = Time.time;
+	}
+		
+	private void UpdateHud() {
+		uiInterface.UpdateHud(totalSheep, sheepInGoal, deadSheep);
+	}
+
+	[PunRPC]
+	void ShowEndScreen() {
+		SetPause(true);
+		uiInterface.ShowEndScreen(totalSheep, sheepInGoal, deadSheep, Time.time - startTime);
+	}
+
+	public List<GameObject> GetEntities(bool refresh = false) {
+		if (entities == null || refresh) {
+			Debug.Log ("Rebuilding Entities");
+
+			entities = new List<GameObject> ();
+
+			entities.AddRange (GameObject.FindGameObjectsWithTag ("entity"));
+			// May need to filter innactive.
+			entities.AddRange(GameObject.FindGameObjectsWithTag("Player"));
+		}
+
+		return entities;
+	}
+
+	private void CheckEnd() {
+		if (totalSheep - sheepInGoal - deadSheep == 0) {
+			SetPause(true);
+			//Show EndScreen
+		}
+	}
+		
+	private void SetPause(bool paused) {
+		this.paused = paused;
+		Time.timeScale = paused ? 0f : 1f;
+	}
+}
+
---
+++ Assets/Scripts/Mono/Sheep/SheepDisplay.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public interface ISheepDisplay {
+	SheepState GetSheepState();
+}
+
+public class SheepDisplay : MonoBehaviour {
+	public SheepState sheepState = SheepState.Idle;
+
+	private Renderer rendComp;
+	private ISheepDisplay iSheepDisplay;
+
+	public void Start() {
+		rendComp = GetComponentInChildren<Renderer> ();
+		iSheepDisplay = GetComponent<ISheepDisplay> ();
+	}
+
+	// Update is called once per frame
+	public void Update () {
+		ColorizeBehavior ();
+	}
+
+	private void ColorizeBehavior() {
+		Color color;
+
+		sheepState = iSheepDisplay.GetSheepState ();
+
+		switch (sheepState) {
+		case SheepState.Wandering:
+			color = Color.yellow;
+			break;
+		case SheepState.Herding:
+			color = Color.blue;
+			break;
+		case SheepState.Panicking:
+			color = Color.red;
+			break;
+		case SheepState.Eating:
+			color = Color.green;
+			break;
+		default:
+			color = Color.black;
+			break;
+		}
+
+		rendComp.material.color = color;
+	}
+}
